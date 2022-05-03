45f81c76b9c3d2acf04963672efeee0691c3f3f1
Virag Savaliya <67185877+viragsavaliya@users.noreply.github.com>
2022-05-03 22:30:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById('movieoption1').onclick = function() {
+   document.getElementById('movieoption1').style.backgroundColor = '#DC354599';
+   document.getElementById('movieoption1').style.color = '#FFF';
+}
+document.getElementById('movieoption2').onclick = function() {
+   document.getElementById('movieoption2').style.backgroundColor = '#DC354599';
+   document.getElementById('movieoption2').style.color = '#FFF';
+}
+document.getElementById('movieoption3').onclick = function() {
+   document.getElementById('movieoption3').style.backgroundColor = '#19875499';
+   document.getElementById('movieoption3').style.color = '#FFF';
+}
+document.getElementById('movieoption4').onclick = function() {
+   document.getElementById('movieoption4').style.backgroundColor = '#DC354599';
+   document.getElementById('movieoption4').style.color = '#FFF';
+}
+
+
+document.getElementById('songoption1').onclick = function() {
+   document.getElementById('songoption1').style.backgroundColor = '#19875994';
+   document.getElementById('songoption1').style.color = '#FFF';
+}
+document.getElementById('songoption2').onclick = function() {
+   document.getElementById('songoption2').style.backgroundColor = '#DC354599';
+   document.getElementById('songoption2').style.color = '#FFF';
+}
+document.getElementById('songoption3').onclick = function() {
+   document.getElementById('songoption3').style.backgroundColor = '#DC354599';
+   document.getElementById('songoption3').style.color = '#FFF';
+}
+document.getElementById('songoption4').onclick = function() {
+   document.getElementById('songoption4').style.backgroundColor = '#DC354599';
+   document.getElementById('songoption4').style.color = '#FFF';
+}
+
+
+document.getElementById('gamesoption1').onclick = function() {
+   document.getElementById('gamesoption1').style.backgroundColor = '#DC354599';
+   document.getElementById('gamesoption1').style.color = '#FFF';
+}
+document.getElementById('gamesoption2').onclick = function() {
+   document.getElementById('gamesoption2').style.backgroundColor = '#DC354599';
+   document.getElementById('gamesoption2').style.color = '#FFF';
+}
+document.getElementById('gamesoption3').onclick = function() {
+   document.getElementById('gamesoption3').style.backgroundColor = '#DC354599';
+   document.getElementById('gamesoption3').style.color = '#FFF';
+}
+document.getElementById('gamesoption4').onclick = function() {
+   document.getElementById('gamesoption4').style.backgroundColor = '#19875499';
+   document.getElementById('gamesoption4').style.color = '#FFF';
+}
